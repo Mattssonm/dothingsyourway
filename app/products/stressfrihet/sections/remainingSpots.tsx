5e5777ff478f4700d73c3f019bb9ec9c979a3815
Matilda Mared <matildamared@live.se>
2023-05-10 19:48:55 +0200

    add remaining spots component

---
+++ app/products/stressfrihet/sections/remainingSpots.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+interface RemainingSpotsData {
+	hero_id: number;
+	redirect: boolean;
+	provider: String;
+	total: number;
+	remaining: number;
+	taken: number;
+}
+
+const RemainingSpots = () => {
+	const [data, setData] = useState<RemainingSpotsData>();
+
+	useEffect(() => {
+		const fetchRemainingSpotsData = async () => {
+			const url = window.location.href;
+			const response = await fetch(
+				`https://automatehero.io/api/scarcity/ah_sdt_hero_UkJDTG85TFdRd1RX?sdt_scarcity_host=${url}`,
+				{
+					headers: {
+						accept: "*/*",
+						"accept-language": "sv-SE,sv;q=0.9,en-US;q=0.8,en;q=0.7",
+						"sec-ch-ua":
+							'"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+						"sec-ch-ua-mobile": "?0",
+						"sec-ch-ua-platform": '"macOS"',
+						"sec-fetch-dest": "empty",
+						"sec-fetch-mode": "cors",
+						"sec-fetch-site": "cross-site",
+					},
+					referrer: url,
+					referrerPolicy: "strict-origin-when-cross-origin",
+					body: null,
+					method: "GET",
+					mode: "cors",
+					credentials: "omit",
+				}
+			);
+			const data: { data: RemainingSpotsData } = await response.json();
+			setData(data.data);
+		};
+
+		fetchRemainingSpotsData();
+	}, []);
+
+	return (
+		<>
+			{data && (
+				<>
+					<h1>
+						Det finns just nu {data.remaining} platser kvar av {data.total}{" "}
+						möjliga!
+					</h1>
+				</>
+			)}
+		</>
+	);
+};
+
+export default RemainingSpots;
--- app/products/stressfrihet/sections/testimonials.tsx
+++ app/products/stressfrihet/sections/testimonials.tsx
@@ -8,7 +8,13 @@ const Testimonials = () => {
 				Det här säger tidigare kunder:
 			</h2>
 			<div className="w-full max-w-4xl">
-				<div><iframe src="https://player.vimeo.com/video/725239814?h=b6e83b9021&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Jessica Testimonial"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script> 
+				<div>
+					<iframe
+						src="https://player.vimeo.com/video/725239814?h=b6e83b9021&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+						allowFullScreen
+						title="Jessica Testimonial"
+					></iframe>
+				</div>
 			</div>
 			<p>!SNEKY resten av datan ligger i testimonials.tsx</p>
 		</Section>
@@ -19,43 +25,43 @@ const testimonials: Testimonial[] = [
 	{
 		name: "Lisa Möller",
 		image: "/images/testimonials/lisa_moller.jpeg",
-		text: 'Kursen Stressfrihet har gett mig många insikter, inspiration och verktyg att tänka i nya banor och ifrågasätta "sanningar" om mig själv och omvärlden. Både föreläsningarna, gruppcoachningarna, övningarna och det digitala verktyget har varit hjälpsamt på vägen mot förändring. Markus är dessutom en lyhörd och generös coach och kursledare som verkligen brinner för att genuint hjälpa andra och sprida sin kunskap och sina erfarenheter! Tack för en kurs som gett mig verktyg för livet! ⭐️🙏'
+		text: 'Kursen Stressfrihet har gett mig många insikter, inspiration och verktyg att tänka i nya banor och ifrågasätta "sanningar" om mig själv och omvärlden. Både föreläsningarna, gruppcoachningarna, övningarna och det digitala verktyget har varit hjälpsamt på vägen mot förändring. Markus är dessutom en lyhörd och generös coach och kursledare som verkligen brinner för att genuint hjälpa andra och sprida sin kunskap och sina erfarenheter! Tack för en kurs som gett mig verktyg för livet! ⭐️🙏',
 	},
 	{
 		name: "Katja Tuovila",
 		image: "/images/testimonials/katja_tuovila.jpeg",
-		text: "Kursen Stressfrihet har varit riktigt bra! 🌟 fantastiskt! ❤️ Lärt mig en hel del, fått nya insikter/sätt verktyg i varje del man går igenom som man alltid kommer ha med sig för livet! Kursen har betytt väldigt mycket för mig av flera anledningar! ❤️ Det har hjälpt mig enormt! Känner mig starkare, modigare m.m. Tacksam att jag fick chansen att vara med och tackade ja trots lite nervositet inför det nya! Kan absolut rekommendera till andra att gå! Ger väldigt mycket och som man har nytta av för alltid! ❤️"
+		text: "Kursen Stressfrihet har varit riktigt bra! 🌟 fantastiskt! ❤️ Lärt mig en hel del, fått nya insikter/sätt verktyg i varje del man går igenom som man alltid kommer ha med sig för livet! Kursen har betytt väldigt mycket för mig av flera anledningar! ❤️ Det har hjälpt mig enormt! Känner mig starkare, modigare m.m. Tacksam att jag fick chansen att vara med och tackade ja trots lite nervositet inför det nya! Kan absolut rekommendera till andra att gå! Ger väldigt mycket och som man har nytta av för alltid! ❤️",
 	},
 	{
 		name: "Ulrica Nilsson",
 		image: "/images/testimonials/ulrica_nilsson.jpeg",
-		text: "Jag har varit med på Markus kurs i stressfrihet och kan varmt rekommendera den. Markus förklarar efter egen erfarenhet och på ett grymt proffsigt och ödmjukt sätt hur du kan förhålla dig till stress. Jag är så tacksam för Markus som givit mig så mycket klarhet och ett lugn. Kursen är uppbyggd på ett konkret och mycket väl genomtänkt koncept. Ge dig själv gott om tid med kursen när du väljer DIG för att du är så värdefull."
+		text: "Jag har varit med på Markus kurs i stressfrihet och kan varmt rekommendera den. Markus förklarar efter egen erfarenhet och på ett grymt proffsigt och ödmjukt sätt hur du kan förhålla dig till stress. Jag är så tacksam för Markus som givit mig så mycket klarhet och ett lugn. Kursen är uppbyggd på ett konkret och mycket väl genomtänkt koncept. Ge dig själv gott om tid med kursen när du väljer DIG för att du är så värdefull.",
 	},
 	{
 		name: "Mari Johansson",
 		image: "/images/testimonials/mari_johansson.jpg",
-		text: "Programmet har givit mig så mycket mer än att fokusera på företaget. Jag har fått bra verktyg till planering, bra tips för utnyttja dagens timmar så att energin kan läggas på rätt saker och dessutom självutveckling som person. Min energi har höjts och jag kan välja att utnyttja ev stress till det positiva. Jag rekommenderar varmt detta program och coachningen från Markus. TIden har bara rusat iväg och jag har sett fram emot varje veckas möten. Nog kan jag säga att jag har blivit en version högre av mig själv. Väl värd investering!!"
+		text: "Programmet har givit mig så mycket mer än att fokusera på företaget. Jag har fått bra verktyg till planering, bra tips för utnyttja dagens timmar så att energin kan läggas på rätt saker och dessutom självutveckling som person. Min energi har höjts och jag kan välja att utnyttja ev stress till det positiva. Jag rekommenderar varmt detta program och coachningen från Markus. TIden har bara rusat iväg och jag har sett fram emot varje veckas möten. Nog kan jag säga att jag har blivit en version högre av mig själv. Väl värd investering!!",
 	},
 	{
 		name: "Marie Meyer",
 		image: "/images/testimonials/marie_meyer.jpeg",
-		text: "När jag började Stressfrihet så slet jag ihjäl mig på mitt jobb. Och när jag tog rimliga pauser så kände jag ofta en skuld över att jag inte jobbade. Jag höll på att gå upp i tid på jobbet och jag orkade inte med min fritid. Samtidigt höll jag också på att starta upp mitt eget företag. Markus material i kursen gjorde det verkligen möjligt för mig att ändra mitt mindset till stress, struktur och värde. Jag började prioritera mig själv mycket mer och fick verktyg för att hantera ångesten av att inte prestera. Detta gjorde det möjligt att få loss energi till det jag faktiskt verkligen ville göra och satsa på. Väldigt hjälpsam kurs!"
+		text: "När jag började Stressfrihet så slet jag ihjäl mig på mitt jobb. Och när jag tog rimliga pauser så kände jag ofta en skuld över att jag inte jobbade. Jag höll på att gå upp i tid på jobbet och jag orkade inte med min fritid. Samtidigt höll jag också på att starta upp mitt eget företag. Markus material i kursen gjorde det verkligen möjligt för mig att ändra mitt mindset till stress, struktur och värde. Jag började prioritera mig själv mycket mer och fick verktyg för att hantera ångesten av att inte prestera. Detta gjorde det möjligt att få loss energi till det jag faktiskt verkligen ville göra och satsa på. Väldigt hjälpsam kurs!",
 	},
 	{
 		name: "Sabine Kargl",
 		image: "/images/testimonials/sabine_kargl.jpeg",
-		text: "Jag kan varmt rekommendera Markus kurs Stressfrihet. När jag började kursen var jag utbränd och helt sönderstressad. Livet var upp och ner. Genom kursens gång fick jag ordning på både knopp och kropp och ser nu även stressen som motivation och drivkraft som jag styr över. Helt ovärderligt! Tack Markus 🙏"
+		text: "Jag kan varmt rekommendera Markus kurs Stressfrihet. När jag började kursen var jag utbränd och helt sönderstressad. Livet var upp och ner. Genom kursens gång fick jag ordning på både knopp och kropp och ser nu även stressen som motivation och drivkraft som jag styr över. Helt ovärderligt! Tack Markus 🙏",
 	},
 	{
 		name: "Selma Subasic",
 		image: "/images/testimonials/avatar.jpeg",
-		text: "När jag väl började detta programmet hade jag helt ärligt inte jätte mycket förväntningar. Jag trodde mest att Markus skulle hjälpa mig att nå mina mål i företagandet. Men under hela denna tiden har jag fått så mycket mer. Jag har fått hjälp att öppna dörrar som jag verkligen har behövt öppna för att kunna ta mig vidare i mitt liv. Jag levde i konstant stress och ororade mig ständigt utan att veta varför.  Med Markus hjälp tog denna kursen mig flera steg framåt i rätt riktning! Han lärde mig hur jag ska hitta tillbaka till mig själv, vilket inte går att sätta något pris på! ❤️"
+		text: "När jag väl började detta programmet hade jag helt ärligt inte jätte mycket förväntningar. Jag trodde mest att Markus skulle hjälpa mig att nå mina mål i företagandet. Men under hela denna tiden har jag fått så mycket mer. Jag har fått hjälp att öppna dörrar som jag verkligen har behövt öppna för att kunna ta mig vidare i mitt liv. Jag levde i konstant stress och ororade mig ständigt utan att veta varför.  Med Markus hjälp tog denna kursen mig flera steg framåt i rätt riktning! Han lärde mig hur jag ska hitta tillbaka till mig själv, vilket inte går att sätta något pris på! ❤️",
 	},
 	{
 		name: "Frida Edmark",
 		image: "/images/testimonials/avatar.jpeg",
-		text: "Är mycket nöjd med programmet. Modulerna har varit mycket givande och det har varit guld att få mötas veckovis för gruppcoaching och inspirerande samtal. Programmet har stärkt mig mentalt och givit mig en bättre grund för att kunna driva företag."
-	}
+		text: "Är mycket nöjd med programmet. Modulerna har varit mycket givande och det har varit guld att få mötas veckovis för gruppcoaching och inspirerande samtal. Programmet har stärkt mig mentalt och givit mig en bättre grund för att kunna driva företag.",
+	},
 ];
 
 export default Testimonials;
