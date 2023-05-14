f9bc2448ff8030967c78726cdc71f5d972f9926e
Markus Mattsson <markusmattsson90@gmail.com>
2023-05-14 23:20:02 +0200

    add earlybird pages

--- app/products/stressfrihet/early/page.tsx
+++ app/products/stressfrihet/early/page.tsx
@@ -1,7 +1,39 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import FAQSection from "../sections/faq";
+import About from "@/components/About";
+import ModulesSection from "../sections/modules";
+import TestimonialsSection from "../sections/testimonials";
+import HeroSection from "../sections/hero";
+import VideoSection from "../sections/video";
+import RecognizeSection from "../sections/recognize";
+import TimeAndEnergySection from "../sections/timeAndEnergy";
+import MethodSection from "../sections/method";
+import HowToSection from "../sections/howTo";
+import MindsetSection from "../sections/mindset";
+import PurchaseSection from "./purchaseEarly";
+
+export const metadata = {
+	title: "Do Things Your Way | Stressfrihet",
+};
+
 export default function Page() {
 	return (
-		<div>
-			<h1>Stressfrihet Early</h1>
-		</div>
+		<>
+			<Header variant="light" />
+			<HeroSection />
+			<VideoSection />
+			<RecognizeSection />
+			<TimeAndEnergySection />
+			<About />
+			<MethodSection />
+			<TestimonialsSection />
+			<HowToSection />
+			<ModulesSection />
+			<PurchaseSection />
+			<MindsetSection />
+			<FAQSection />
+			<Footer />
+		</>
 	);
 }
---
+++ app/products/stressfrihet/early/purchaseEarly.tsx
@@ -0,0 +1,99 @@
+import Button from "@/components/Button";
+import Section from "@/components/Section";
+import Image from "next/image";
+import RemainingSpots from "../sections/remainingSpots";
+
+const PurchaseSection = () => {
+	return (
+		<Section
+			className="flex flex-col items-center bg-secondary-dark gap-16"
+			id="purchase"
+		>
+			<h2 className="font-display text-white text-3xl md:text-5xl uppercase text-center">
+				Bli fri från stress idag!
+			</h2>
+			<Image
+				src="/images/organizer.svg"
+				width={400}
+				height={250}
+				alt="Illustration of a woman and a man sitting in chairs discussing"
+			/>
+			<div className="bg-background p-16 rounded-2xl flex flex-col gap-4 max-w-screen-md items-center text-left">
+				<p className="text-xs uppercase text-gray-500 tracking-wide">
+					Stressfrihet - 12-veckorsprogrammet
+				</p>
+				<Image
+					className="rounded-xl"
+					width="400"
+					height="150"
+					src="https://automatehero.io/api/path/UkJ1TW85TFdRd1RX/gif"
+					alt="countdown"
+				/>
+				<h3>Sista dag för anmälan är den 4 Juni!</h3>
+				<h3 className="font-display text-xl md:text-2xl w-full text-secondary-darkest">
+					Upplägg
+				</h3>
+				<p>
+					Programmet består av sex delar och pågår under 12 veckor. Du får
+					tillgång till alla videor och övningar idag och sen ses vi varje vecka
+					för gruppcoaching. Där får du chans och ställa frågor och få coaching
+					så att du direkt ska kunna tillämpa dina nya kunskaper.
+				</p>
+				<h3 className="font-display text-xl md:text-2xl w-full text-secondary-darkest">
+					När sätter det igång?
+				</h3>
+				<p>
+					Kurstart är den <span className="font-bold">tisdagen den 13 Juni</span>. Alla tillfällen är på tisdagar
+					mellan kl 18:00 – 19:15. Vecka 29 och 30 är det semesteruppehåll. Därför blir det sista tillfället den 12 September.
+				</p>
+				<h3 className="font-display text-xl md:text-2xl w-full text-secondary-darkest">
+					Vad du får:
+				</h3>
+				<ul className="list-disc ml-8 flex flex-col gap-4">
+					<li>
+						Sex moduler med tillhörande arbetsmaterial (pdf:er, övningar m.m.) 
+						<span className="italic font-bold">(värde 16 000 kr)</span>
+					</li>
+					<li>
+						12 gruppcoachningstillfällen 
+						<span className="italic font-bold">(värde 15 000 kr)</span>
+					</li>
+					<li>
+						Tre månaders tillgång till sluten Facebookgrupp där där du kan få
+						svar på dina frågor och där vi gemensamt pushar varandra{" "}
+						<span className="italic font-bold">(värde 6000 kr)</span>
+					</li>
+					<li>
+						<span className="font-bold">BONUS 1!</span> - 
+						En kopia av mitt moderna och avancerade affärssystem som jag själv
+						använder för mitt företagande, min personliga utveckling och
+						produktivitet. Detta system får du behålla för alltid och jag kommer
+						lära dig hur du anpassar det efter dina behov 
+						<span className="italic font-bold">(värde 15 000 kr)</span>
+					</li>
+					<li>
+						<span className="font-bold">BONUS 2!!</span> – Dessutom kommer du få videoserien {`"Hållbara Dagar"`}, som
+						består av tre videor på sammanlagt en timme som lär dig allt om hur
+						du skapar hållbarhet i din energi och ekonomi 
+						<span className="italic font-bold">(värde 2000 kr)</span>
+					</li>
+				</ul>
+				<h3 className="font-bold">Totalt värde: 54 000 kr</h3>
+
+				<h3 className="font-bold mt-10 text-xl md:text-2xl "> Du betalar endast: <span className="text-red-600">7970 kr</span></h3>
+				<Button className="mb-10"
+					variant="secondary"
+					type="link"
+					href="https://automatehero.io/api/path/UkJDTG85TFdRd1RW"
+					target="_blank"
+				>
+					Reservera min plats nu
+				</Button>
+
+				<RemainingSpots />
+			</div>
+		</Section>
+	);
+};
+
+export default PurchaseSection;
