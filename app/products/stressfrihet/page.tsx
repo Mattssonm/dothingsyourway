65094eb6b1bdd82199c04ff906b3af9caa0a1cbd
Matilda Mared <matildamared@live.se>
2023-05-14 11:57:06 +0200

    refactor hero section into its own component

--- app/products/stressfrihet/page.tsx
+++ app/products/stressfrihet/page.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import CourseContent from "./sections/courseContent";
 import Testimonials from "./sections/testimonials";
 import RemainingSpots from "./sections/remainingSpots";
+import HeroSection from "./sections/hero";
 
 export const metadata = {
 	title: "Do Things Your Way | Stressfrihet",
@@ -17,29 +18,7 @@ export default function Page() {
 	return (
 		<>
 			<Header variant="light" />
-			<Section className="bg-primary-dark text-text-light text-right md:min-h-screen items-center flex flex-col md:flex-row space-x-8 space-y-16">
-				<div>
-					<Image
-						src="/images/organizing.svg"
-						width={800}
-						height={600}
-						alt="Organizing"
-					/>
-				</div>
-				<div className="flex flex-col justify-center items-end space-y-4">
-					<h2 className="text-4xl md:text-6xl font-display uppercase">
-						Stressfrihet
-					</h2>
-					<h3 className="font-display text-xl md:text-2xl">
-						Få mer gjort och må fantastiskt bra utan att stressa mindre
-					</h3>
-					<p className="font-thin italic w-96">
-						En 12-veckors kurs där du lär dig förvandla all negativ stress till
-						positiv genom att installera ett nytt mindset
-					</p>
-					<Button>Ja, jag vill bli fri från negativ stress!</Button>
-				</div>
-			</Section>
+			<HeroSection />
 			<Section className="bg-primary-darkest flex flex-col items-center justify-center md:min-h-screen">
 				<iframe
 					title="vimeo-player"
---
+++ app/products/stressfrihet/sections/hero.tsx
@@ -0,0 +1,33 @@
+import Section from "@/components/Section";
+import Button from "@/components/Button";
+import Image from "next/image";
+
+const HeroSection = () => {
+	return (
+		<Section className="bg-primary-dark text-text-light text-right md:min-h-screen items-center flex flex-col md:flex-row space-x-8 space-y-16">
+			<div>
+				<Image
+					src="/images/organizing.svg"
+					width={800}
+					height={600}
+					alt="Organizing"
+				/>
+			</div>
+			<div className="flex flex-col justify-center items-end space-y-4">
+				<h2 className="text-4xl md:text-6xl font-display uppercase">
+					Stressfrihet
+				</h2>
+				<h3 className="font-display text-xl md:text-2xl">
+					Få mer gjort och må fantastiskt bra utan att stressa mindre
+				</h3>
+				<p className="font-thin italic w-96">
+					En 12-veckors kurs där du lär dig förvandla all negativ stress till
+					positiv genom att installera ett nytt mindset
+				</p>
+				<Button>Ja, jag vill bli fri från negativ stress!</Button>
+			</div>
+		</Section>
+	);
+};
+
+export default HeroSection;
